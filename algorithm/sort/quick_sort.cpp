e5dcd4522a5b51db8d4af7c3f3eee824d65bd930
Huu Nguyen <thieuvan0405@gmail.com>
2021-03-03 22:59:22 +0700

    Add quick sort

--- algorithm/sort/quick_sort.cpp
+++ algorithm/sort/quick_sort.cpp
@@ -1,5 +1,11 @@
 #include "sort.h"
 
+template <typename T>
+int partition(T *&arr, int l, int r);
+
+template <typename T>
+void recursiveQuickSort(T *&arr, int l, int r);
+
 int main(int argc, char const *argv[])
 {
     int size;
@@ -11,11 +17,45 @@ int main(int argc, char const *argv[])
         cin >> arr[i];
     }
     quick_sort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout<<endl;
     return 0;
 }
 
 template <typename T>
-void quick_sort(T *&arr, uint32_t size)
+int partition(T *&arr, int l, int r)
 {
+    int pivot = arr[r];
+    int storeIndex = l;
+    for (int i = l; i < r; i++)
+    {
+        if (arr[i] < pivot)
+        {
+            swap_arr(arr[i], arr[storeIndex]);
+            storeIndex++;
+        }
+
+    }
+    swap_arr(arr[storeIndex], arr[r]);
+    return storeIndex;
     
+}
+
+template <typename T>
+void recursiveQuickSort(T *&arr, int l, int r)
+{
+    if (l >= r)
+        return;
+    int pivot = partition(arr, l, r);
+    recursiveQuickSort(arr, l, pivot - 1);
+    recursiveQuickSort(arr, pivot + 1, r);
+}
+
+template <typename T>
+void quick_sort(T *&arr, int size)
+{
+    recursiveQuickSort(arr, 0, size - 1);
 }
